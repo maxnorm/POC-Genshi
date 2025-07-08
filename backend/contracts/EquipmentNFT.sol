da0938f67828f53dce97d3e99c1f9c9075d1350c
Maxime Normandin <web2@londerosports.com>
2025-07-08 16:34:39 -0400

    add equipment nft

--- backend/contracts/EquipmentNFT.sol
+++ backend/contracts/EquipmentNFT.sol
@@ -6,14 +6,14 @@ import {ERC998TopDown} from "./cNFT/ERC998TopDown.sol";
 contract AssemblyNFT is ERC998TopDown {
     uint256 private _count;
 
-    event MintedAssembly(address indexed to, uint256 indexed tokenId);
+    event MintedEquipment(address indexed to, uint256 indexed tokenId);
 
-    constructor() ERC998TopDown("AssemblyNFT", "ASSEMBLY") {}
+    constructor() ERC998TopDown("EquipmentNFT", "EQUIPMENT") {}
 
     function mint(address to) public {
         _count++;
         uint256 tokenId = _count;
         _mint(to, tokenId);
-        emit MintedAssembly(to, tokenId);
+        emit MintedEquipment(to, tokenId);
     }
 }
--- backend/contracts/PieceNFT.sol
+++ backend/contracts/PieceNFT.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
