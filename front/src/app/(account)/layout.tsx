083007aa5bdfe1912fc153812aec61637ac5f1a2
Maxime Normandin <maxnorm@hotmail.com>
2025-07-17 17:34:17 -0400

    remove useUser to build

--- front/src/app/(account)/layout.tsx
+++ front/src/app/(account)/layout.tsx
@@ -1,15 +1,12 @@
 import Auth from "@/components/Auth";
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
-import { UserProvider } from "@/contexts/useUser";
 
 function AccountLayout({ children }: { children: React.ReactNode }) {
   return (
     <Auth>
-      <UserProvider>
         <DashboardLayout>
           {children}
         </DashboardLayout>
-      </UserProvider>
     </Auth>
   );
 }
